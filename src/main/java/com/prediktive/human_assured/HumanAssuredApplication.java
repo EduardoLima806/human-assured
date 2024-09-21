c770ceadb2e015fa73a39a25583a4db935c94301
Eduardo Lima <eduardostudio@gmail.com>
2024-09-21 19:04:11 -0300

    feat: inital commit with project setup

---
+++ src/main/java/com/prediktive/human_assured/HumanAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.prediktive.human_assured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanAssuredApplication.class, args);
+	}
+
+}
